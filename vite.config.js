e64b950809a0b7d9256c1b36ab990379262014f4
LHABIB-TRUSTANGLE <lhabib@trustangle.com>
2023-12-20 18:05:26 +0530

    Updated config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+  server: {
+    proxy: {
+      // Add any proxy configurations if needed
+    },
+    // This configuration handles client-side routing and supports page refreshes
+    fs: {
+      strict: false,
+    },
+    hmr: {
+      overlay: false,
+    },
+  },
+});
